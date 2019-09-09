0d4d4bcba5ac762dafad79f5507483b1b7270c47
Jonathan Azali <azali.jonathan@gmail.com>
2019-09-09 11:14:12 -0700

    updated env and index.js in db

--- db/index.js
+++ db/index.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 
 const pg = require('pg');
 
-// pick variables from .env
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-// construct our connection string
-const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 // create a pg Pool Object
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
